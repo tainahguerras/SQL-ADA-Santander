36a2930609f4ea2c224140e9aeb71afa6d2bd131
OtavioSotnas <otaviosotnas@gmail.com>
2024-03-13 17:09:47 -0300

    TEXT para VARCHAR

--- comandos.sql
+++ comandos.sql
@@ -1,40 +1,40 @@
 CREATE TABLE categorias 
 ( 
  id_categoria INTEGER PRIMARY KEY,  -- Identificador único para cada categoria
- nome_categoria TEXT  -- Nome da categoria
+ nome_categoria VARCHAR(255)  -- Nome da categoria
 );
 
 CREATE TABLE clientes 
 ( 
  id_cliente INTEGER PRIMARY KEY,  -- Identificador único para cada cliente
- nome_cliente TEXT,  -- Nome do cliente
+ nome_cliente VARCHAR(255),  -- Nome do cliente
  idade INTEGER,  -- Idade do cliente
- endereco TEXT  -- Endereço do cliente
+ endereco VARCHAR(255)  -- Endereço do cliente
 );
 
 CREATE TABLE fornecedores 
 ( 
  id_fornecedor INTEGER PRIMARY KEY,  -- Identificador único para cada fornecedor
- nome TEXT,  -- Nome do fornecedor
- contato TEXT  -- Contato do fornecedor (pode ser telefone, email, etc.)
+ nome VARCHAR(255),  -- Nome do fornecedor
+ contato VARCHAR(255)  -- Contato do fornecedor (pode ser telefone, email, etc.)
 );
 
 CREATE TABLE marcas 
 ( 
- id_marca TEXT PRIMARY KEY,  -- Identificador único para cada marca
- nome TEXT  -- Nome da marca
+ id_marca INTEGER PRIMARY KEY,  -- Identificador único para cada marca
+ nome VARCHAR(255)  -- Nome da marca
 );
 
 CREATE TABLE produtos 
 ( 
  id_produto INTEGER PRIMARY KEY,  -- Identificador único para cada produto
- nome_produto TEXT,  -- Nome do produto
+ nome_produto VARCHAR(255),  -- Nome do produto
  preco REAL,  -- Preço do produto
  categoria_id INTEGER,  -- Referência à categoria do produto
- marca_id TEXT,  -- Referência à marca do produto
+ marca_id VARCHAR(255),  -- Referência à marca do produto
  fornecedor_id INTEGER,  -- Referência ao fornecedor do produto
  data_estoque DATE,  -- Data de entrada do produto no estoque
- status TEXT,  -- Status atual do produto (por exemplo, disponível, esgotado)
+ status VARCHAR(255),  -- Status atual do produto (por exemplo, disponível, esgotado)
  FOREIGN KEY(categoria_id) REFERENCES categorias (id_categoria),  -- Chave estrangeira para a tabela Categorias
  FOREIGN KEY(marca_id) REFERENCES marcas (id_marca),  -- Chave estrangeira para a tabela Marcas
  FOREIGN KEY(fornecedor_id) REFERENCES fornecedores (id_fornecedor)  -- Chave estrangeira para a tabela Fornecedores
